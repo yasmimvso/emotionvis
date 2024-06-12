a77230d945560061ce35580ac564d1067723e17d
yasmimvso <yvso@cin.ufpe.br>
2024-06-12 10:42:28 -0300

    feat: icone de background modo claro/escuro criado

--- src/app/component/tamplate/tamplate.component.ts
+++ src/app/component/tamplate/tamplate.component.ts
@@ -7,6 +7,11 @@ import { HeaderService } from '../../services/header.service'
   styleUrls: ['./tamplate.component.css']
 })
 export class TamplateComponent implements OnInit{
+    changeColor : boolean  = true;
+  /*
+      background-color: rgb(21, 32, 43);
+    color: #ccc;
+  */
 
     displayNav: boolean = false;
     @Input() idVis:any;
@@ -28,4 +33,19 @@ export class TamplateComponent implements OnInit{
 
   }
 
+  invertColor(){
+    const element = <HTMLElement> document.getElementsByClassName('alphavis')[0];
+    const head = <HTMLElement> document.getElementsByClassName('header')[0];
+    const element2 = <HTMLElement> document.getElementsByClassName('view')[0];
+    const element3 = <HTMLElement> document.getElementsByClassName(' dadosBygraph')[0];
+    console.log("changeColor", this.changeColor);
+    element.style.background = 'rgb(21, 32, 43)'; // #38444d
+    element.style.color = "rgb(255,255,255)";
+    head.style.background ='#38444d'
+    head.style.color = "rgb(255,255,255)";
+    element2.style.background ='#38444d';
+    element2.style.color = "rgb(255,255,255)";
+    element3.style.background ='#38444d'
+    element3.style.color = "rgb(255,255,255)";
+  }
 }
